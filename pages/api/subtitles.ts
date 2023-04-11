4f203909f496272fb71aefef54f3ad6f7df84979
Hayden Chen <brooklyn1984@qq.com>
2023-04-11 13:19:03 +0800

    chore: :hammer: add log

--- pages/api/subtitles.ts
+++ pages/api/subtitles.ts
@@ -12,11 +12,15 @@ export async function getYoutubeSubtitleUrls(videoId: string) {
       "Content-Type": "text/plain",
       "User-Agent":
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-      "X-Auth-Token": `${process.env.SAVESUBS_X_AUTH_TOKEN}` || "",
+      "X-Auth-Token": process.env.SAVESUBS_X_AUTH_TOKEN || "",
       "X-Requested-Domain": "savesubs.com",
       "X-Requested-With": "xmlhttprequest",
     },
   })
+  console.log(
+    "[faiz:] === SAVESUBS_X_AUTH_TOKEN",
+    process.env.SAVESUBS_X_AUTH_TOKEN
+  )
   console.log("[faiz:] === response", response)
   const { response: json = {} } = await response.json()
   // {
